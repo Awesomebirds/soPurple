c90d1118f30eacbe5d9b7b09866ea6945974208a
Awesomebirds <dado2472@gmail.com>
2021-02-10 18:19:18 +0900

    태그 필터 추가

--- src/Routes/Cocktail/Item.js
+++ src/Routes/Cocktail/Item.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { firestoreService, storageService } from "myFirebase";
+import { storageService } from "myFirebase";
 import { useEffect, useState } from "react";
 
 const Item = ({ cocktail }) => {
